5fd4671e39076408e176c160a57a3cc265493a85
HannWeb <hanner.a.oviedo@hannwebsites.com>
2021-01-06 11:03:13 -0600

    new: initial project structure

---
+++ opendev-coin/blockchain.py
@@ -0,0 +1,15 @@
+class Block:
+    def __init__():
+        pass
+    def calculate_hash():
+        pass
+
+
+class BlockChain:
+    def __init__(self):
+        pass
+    def construct_genesis (self):
+        pass
+        def construct_block(self, proof_no, prev_hash):
+        pass
+
